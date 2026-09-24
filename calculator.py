626f2a912c36326b2d40ad1ae4c8f574a864042f
kiran <tools.vyomanant@gmail.com>
2026-09-24 10:02:53 +0530

    created a local calculator.py

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# Simple Python Calculator
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Cannot divide by zero"
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
+
+# This is a sample comment
